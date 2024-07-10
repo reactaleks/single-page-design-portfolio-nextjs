df916abbc15b324d068e6429b9214514d29237fa
Aleks <a.ivanov.developer@gmail.com>
2024-07-10 11:57:34 +0100

    adjusting image carousel

--- components/PortfolioComponent.tsx
+++ components/PortfolioComponent.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 import React from "react";
 import useEmblaCarousel from "embla-carousel-react";
 
-import {useCallback } from "react";
+import { useCallback } from "react";
 import slideControlLeft from "../public/assets/icon-arrow-left.svg";
 import slideControlRight from "../public/assets/icon-arrow-right.svg";
-import styles from "../components/PortfolioComponent.module.css"
+import styles from "../components/PortfolioComponent.module.css";
 
 export default function PortfolioComponent() {
   const portfolioImages: string[] = [
@@ -21,45 +21,49 @@ export default function PortfolioComponent() {
     align: "start",
     loop: true,
     skipSnaps: false,
-    inViewThreshold: 0.7,
+    inViewThreshold: 0.4,
   });
 
   const onPrevButtonClick = useCallback(() => {
-    console.log('Hello prev')
-    if (!emblaApi) return
-    emblaApi.scrollPrev()
-  }, [emblaApi])
+    if (!emblaApi) return;
+    emblaApi.scrollPrev();
+  }, [emblaApi]);
 
   const onNextButtonClick = useCallback(() => {
-    console.log('Hello next')
-    if (!emblaApi) return
-    emblaApi.scrollNext()
-  }, [emblaApi])
-
- 
+    if (!emblaApi) return;
+    emblaApi.scrollNext();
+  }, [emblaApi]);
 
   return (
-    <div className={` max-w-[48rem] mx-auto`}>
-      <div className={` overflow-hidden`} ref={emblaRef}>
-        <div className={`${styles.embla__container} flex`}>
+    <div className={` max-w-[48rem] flex flex-col justify-center`}>
+      <div className={` overflow-hidden `} ref={emblaRef}>
+        <div className={`${styles.embla__container} flex items-center`}>
           {portfolioImages.map((slide, index) => (
-            <div key={index} className="shrink-0 grow-0 basis-[50%]">
-                <Image src={slide} className="" alt="" width={250} height={250} />
+            <div key={index} className="shrink-0 grow-0 basis-[100%] ">
+              <div className="flex ">
+                <Image
+                  src={slide}
+                  className="w-[540px]"
+                  alt=""
+                  width={540}
+                  height={360}
+                />
+              </div>
             </div>
           ))}
         </div>
       </div>
-      <div className={` h-[64px]`}>
-        <div className={` w-[144px]`}>
+      <div className={``}>
+        <div className={`flex justify-center w-full  h-[64px]`}>
           <Image
             src={slideControlLeft}
-            className={`w-[64] h-[64] bg-black rounded-full  `}
+            className={`w-[64px] h-[64px] bg-black rounded-full  `}
             alt=""
             onClick={onPrevButtonClick}
           />
           <Image
             src={slideControlRight}
-            className={`w-[64] h-[64] bg-black rounded-full  `}
+            className={`w-[64px] h-[64px] bg-black rounded-full  `}
             alt=""
             onClick={onNextButtonClick}
           />
